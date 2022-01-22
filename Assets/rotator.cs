b18071548f7ec7fdc87defe15bed73ef693d7e46
Lukaterasu <lukaross06@gmail.com>
2022-01-22 15:45:26 -0500

    Exercise 2

---
+++ Assets/rotator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    public float rotationSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*
+        if (Input.GetKey(KeyCode.R))
+        {
+            this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            this.GetComponent<Transform>().Rotate(0, -rotationSpeed, 0);
+
+        }
+        */
+        this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+
+    }
+
+    public void Rotate1()
+    {
+       
+            this.GetComponent<Transform>().Rotate(0, rotationSpeed, 0);
+
+        
+        
+    }
+
+    public void Rotate2()
+    {
+        
+            this.GetComponent<Transform>().Rotate(0, -rotationSpeed, 0);
+
+        
+    }
+}
